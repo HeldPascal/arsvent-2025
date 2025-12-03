8499282a8ae2dd894144236d2c2ef35e064d270a
Pascal Held <pascal-held@t-online.de>
2025-12-03 18:50:34 +0100

    Potential fix for build eror

--- backend/src/content/loader.ts
+++ backend/src/content/loader.ts
@@ -77,6 +77,7 @@ export interface PuzzleBlock {
   backgroundImage?: string;
   items?: DragSocketItem[];
   sockets?: DragSocketSlot[];
+   shape?: DragShape;
   solved: boolean;
   solution: unknown;
 }
